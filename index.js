aaf05512c0207341aba4568345345e7924c9b332
cbrand125 <cbrand@talentpath.com>
2019-08-20 14:47:32 -0700

    add functionality

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const gameBoard = document.getElementById('gameBoard');
+const cardArray = [];
+const cardValues = [
+  'king',
+  'queen',
+  'dairy queen',
+  'burger king',
+  'lofi',
+  'robot'
+];
 
-for (let i = 1; i <= 12; i++) {
-  const card = document.createElement('div');
-  card.id = `card${i}`;
-  gameBoard.appendChild(card);
-}
-
+let deck = [...cardValues, ...cardValues];
+resetGame();
 doTimer();
 
 function doTimer() {
@@ -15,10 +20,29 @@ function doTimer() {
       timer.innerHTML = i;
     }, (5 - i) * 1000);
   }
-  setTimeout(myAlert, 5000);
+  setTimeout(myAlert, 5001);
 }
 
 function myAlert() {
   alert('You lose!');
+  resetGame();
   doTimer();
+}
+
+function resetGame() {
+  gameBoard.innerHTML = '';
+  deck = deck.sort(() => Math.random() - 0.5);
+  for (let i = 0; i < 12; i++) {
+    const card = document.createElement('div');
+    card.id = `card${i}`;
+    gameBoard.appendChild(card);
+    cardArray[i] = {
+      id: `card${i}`,
+      value: deck[i],
+      image: '',
+      flipped: false,
+      matched: false
+    };
+  }
+  console.log(cardArray);
 }
